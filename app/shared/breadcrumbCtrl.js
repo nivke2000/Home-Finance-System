24097feeae2395b4020359f98ac1c5566584343e
Niv <nivke2000@yahoo.com>
2017-08-02 13:46:04 +0300

    Working on home page layout

---
+++ app/shared/breadcrumbCtrl.js
@@ -0,0 +1,29 @@
+homeFinanceApp.controller("BreadcrumbCtrl", function($scope) {
+/*    
+    $scope = function () {
+        var homePage = {uiString: "Home", url: "index.html"};
+        var landingPage = {uiString: "Landing", url: "landingPage.html"};
+
+        var breadcrumbArr = [homePage, landingPage];
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+recipeApp.controller("HomeCtrl", function ($scope, $uibModal) {
+    $scope.login = function() {
+        $uibModal.open({
+            templateUrl: "app/login/login.html",
+            controller: "LoginCtrl"
+        })
+    }
+});*/
+});
