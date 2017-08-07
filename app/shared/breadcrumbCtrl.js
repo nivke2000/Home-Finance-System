48885024bb934350973862df8a2874b405bc9a43
niv <nivke2000@yahoo.com>
2017-08-07 15:08:33 +0300

    Working on nav menu

--- app/shared/breadcrumbCtrl.js
+++ app/shared/breadcrumbCtrl.js
@@ -1,8 +1,7 @@
-homeFinanceApp.controller("BreadcrumbCtrl", function($scope, breadcrumbService) {
+homeFinanceApp.controller("MainCtrl", function($scope, breadcrumbService) {
   
     breadcrumbService.clearAll();
-    breadcrumbService.addBreadcrumb ({uiString: "Home", url: "index.html"});
-    breadcrumbService.addBreadcrumb ({uiString: "Landing", url: "landingPage.html"});
-    
+    breadcrumbService.addBreadcrumb ({uiString: "Main page", url: "index.html"});
+        
     $scope.breadcrumbArr = breadcrumbService.getTrail();
 });
